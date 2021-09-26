408926118e34064500055877caf9bb44efd73e89
Yannick Huggler <yannick@huggler.io>
2021-09-26 19:45:42 +0200

    Add playground file for week 1 of the semester

--- week1/main.cpp
+++ week1/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 #define MAX(a, b) ((a) < (b) ? (b) : (a))
+#define GET_MEM_USAGE_OF_TYPE(typeName, type) (std::cout << typeName << ":\t" << sizeof(type) << std::endl)
 
 void postIncrementExperiment() {
     int x = 0;
     int y = x++ + y++;
     std::cout << "Post Increment Challenges" << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
     std::cout << "x = " << x << ", y = " << y << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
 }
 
 void maxWithMacroWithUndefinedBehaviour() {
@@ -17,9 +18,9 @@ void maxWithMacroWithUndefinedBehaviour() {
     int z = MAX(x++, y++);
 
     std::cout << "MAX with Macro with undefined behaviour" << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
     std::cout << "x = " << x << ", y = " << y << ", y = " << z << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
 }
 
 void maxWithMacroWithDefinedBehaviour() {
@@ -32,14 +33,27 @@ void maxWithMacroWithDefinedBehaviour() {
     int z = MAX(x, y);
 
     std::cout << "MAX with Macro with defined behaviour" << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
     std::cout << "x = " << x << ", y = " << y << ", y = " << z << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
+}
+
+void calculateMemoryUsageOfPrimitiveDataTypes() {
+    std::cout << "Memory Usage of primitive data types (in bytes)" << std::endl;
+    std::cout << "===============================================" << std::endl;
+    GET_MEM_USAGE_OF_TYPE("Char", char);
+    GET_MEM_USAGE_OF_TYPE("Short", short);
+    GET_MEM_USAGE_OF_TYPE("Bool", bool);
+    GET_MEM_USAGE_OF_TYPE("Int", int);
+    GET_MEM_USAGE_OF_TYPE("Float", float);
+    GET_MEM_USAGE_OF_TYPE("Double", double);
+    std::cout << "===============================================" << std::endl;
 }
 
 int main() {
     postIncrementExperiment();
     maxWithMacroWithUndefinedBehaviour();
     maxWithMacroWithDefinedBehaviour();
+    calculateMemoryUsageOfPrimitiveDataTypes();
     return 0;
 }
